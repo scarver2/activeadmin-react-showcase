cf49cf1cc45430ca1b626a8604210cdd3a3b5663
Cora <311612359+a1web-cora@users.noreply.github.com>
2026-09-07 08:08:26 -0500

    fix: avoid showcase migration collisions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_09_07_210001) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_07_260001) do
   create_table "accounts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "name", null: false
