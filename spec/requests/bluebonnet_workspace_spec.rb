6eea33413ec3c70b0272d99cf466d093f25a9a16
Cora <311612359+a1web-cora@users.noreply.github.com>
2026-09-21 12:49:01 -0500

    feat: consume Texas Bluebonnet theme composition

--- spec/requests/bluebonnet_workspace_spec.rb
+++ spec/requests/bluebonnet_workspace_spec.rb
@@ -15,7 +15,18 @@ RSpec.describe "Texas Bluebonnet composition prototype" do
     get admin_data_explorer_path, params: { composition: "bluebonnet" }
 
     expect(response).to have_http_status(:ok)
+    expect(response.body).to include('data-activeadmin-theme="texas-bluebonnet"')
     expect(response.body).to include('class="bluebonnet-workspace"')
+    props = JSON.parse(response.parsed_body.at_css('[data-react-component="AccountExplorer"]')["data-react-props"])
+    expect(props.fetch("composition")).to eq(
+      "dataHeading" => "bluebonnet-data-heading",
+      "dataSurface" => "bluebonnet-data-surface",
+      "dataTable" => "bluebonnet-data-table",
+      "pagination" => "bluebonnet-pagination",
+      "primaryAction" => "bluebonnet-primary-action",
+      "toolbar" => "bluebonnet-toolbar",
+      "toolbarSurface" => "bluebonnet-toolbar-surface"
+    )
     expect(response.body).to include('data-react-component="AccountExplorer"')
     expect(response.body).to include('/showcase-icons.svg#heroicons-squares-2x2')
     expect(response.body).to include("Interactive filtering requires JavaScript")
@@ -24,10 +35,12 @@ RSpec.describe "Texas Bluebonnet composition prototype" do
     expect(response.body).not_to include('data-react-component="ThemeSwitcher"')
 
     get admin_data_explorer_path
+    expect(response.body).not_to include('data-activeadmin-theme="texas-bluebonnet"')
     expect(response.body).not_to include('class="bluebonnet-workspace"')
     expect(response.body).to include('data-react-component="ThemeSwitcher"')
 
     get admin_root_path, params: { composition: "bluebonnet" }
+    expect(response.body).not_to include('data-activeadmin-theme="texas-bluebonnet"')
     expect(response.body).not_to include('class="bluebonnet-workspace"')
     expect(response.body).to include('data-react-component="ThemeSwitcher"')
   end
